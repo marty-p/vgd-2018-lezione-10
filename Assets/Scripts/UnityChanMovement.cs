415c5d18ff49556a47bff02dae5727f86d8e4ede
marty-p <37085112+marty-p@users.noreply.github.com>
2018-12-18 16:03:09 +0100

    Add files via upload

---
+++ Assets/Scripts/UnityChanMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanMovement : MonoBehaviour {
+
+    Animator animator;
+    Rigidbody rb;
+    public float speed = 1f;
+    public float velocity = 0f;
+
+	void Start () {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+        float y = Input.GetAxis("Vertical");
+        velocity = y * speed;
+        if (Mathf.Abs(velocity) > float.Epsilon) //if (y != 0)
+        {
+            animator.SetBool("isMoving", true);
+            animator.SetFloat("Velocity", y);
+            rb.velocity = transform.forward * velocity;
+        }
+        else
+            animator.SetBool("isMoving", false);
+    }
+}
