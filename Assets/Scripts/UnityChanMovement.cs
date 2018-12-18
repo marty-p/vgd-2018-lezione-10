2c3715b17e15ab31657203b272213e18686d8ef9
marty-p <37085112+marty-p@users.noreply.github.com>
2018-12-18 16:43:47 +0100

    Add files via upload

--- Assets/Scripts/UnityChanMovement.cs
+++ Assets/Scripts/UnityChanMovement.cs
@@ -7,7 +7,7 @@ public class UnityChanMovement : MonoBehaviour {
     Animator animator;
     Rigidbody rb;
     public float speed = 1f;
-    public float velocity = 0f;
+    public float turnSpeed = 1f;
 
 	void Start () {
         animator = GetComponent<Animator>();
@@ -16,14 +16,26 @@ public class UnityChanMovement : MonoBehaviour {
 	
 	void FixedUpdate () {
         float y = Input.GetAxis("Vertical");
-        velocity = y * speed;
-        if (Mathf.Abs(velocity) > float.Epsilon) //if (y != 0)
+        float x = Input.GetAxis("Horizontal");
+
+        float velocity = y * speed;
+        float turnVelocity = x * turnSpeed;
+
+        if (Mathf.Abs(velocity) > float.Epsilon)
         {
             animator.SetBool("isMoving", true);
             animator.SetFloat("Velocity", y);
+            //Vector3 velocityToApply = new Vector3(0, turnVelocity, 0);
             rb.velocity = transform.forward * velocity;
         }
         else
             animator.SetBool("isMoving", false);
+
+        //if (Mathf.Abs(turnVelocity) > float.Epsilon)
+        {
+            animator.SetFloat("TurnRate", x);
+            Vector3 rotationToApply = new Vector3(0, turnVelocity, 0);
+            rb.rotation = Quaternion.Euler(rb.rotation.eulerAngles + rotationToApply);
+        }
     }
 }
